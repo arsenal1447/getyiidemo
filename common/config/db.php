08ebf52b8ffdd6695d100c769f1d1a9bf4345553
xinxin <xinxin@rrkd.cn>
2016-05-04 09:54:23 +0800

    commit all files

---
+++ common/config/db.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\helpers\ArrayHelper;
+
+$MYSQL_PORT_3306_TCP_ADDR = env('MYSQL_PORT_3306_TCP_ADDR', 'localhost');
+$MYSQL_DB_NAME = env('MYSQL_INSTANCE_NAME', 'getyii');
+$MYSQL_USERNAME = env('MYSQL_USERNAME', 'root');
+$MYSQL_PASSWORD = env('MYSQL_PASSWORD', '123456');
+
+$db = [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host='.$MYSQL_PORT_3306_TCP_ADDR.';dbname='.$MYSQL_DB_NAME,
+    'username' => $MYSQL_USERNAME,
+    'password' => $MYSQL_PASSWORD,
+    'charset' => 'utf8',
+    //'charset' => 'utf8mb4',
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 3600,
+    'schemaCache' => 'cache',
+];
+
+return ArrayHelper::merge(
+    $db,
+    file_exists(__DIR__ . '/db-local.php') ? require(__DIR__ . '/db-local.php') : []
+);
